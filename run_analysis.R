4e5ff5b072e2affd005b74fe5c1f6913e94182d6
bkavalar <bkavalar@sbcglobal.net>
2017-07-31 12:54:13 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 #Final Project - Week 4 for Getting & Cleaning Data for JHU Data Science Course
 #Getting and clearing data from the UCI for Human Activity Recognition Using Smartphones Dataset
-#Prepared by B. Kavalar - 25 July 2017
+#Prepared by B. Kavalar - 31 July 2017 - post submission revision
 
 #set working directory
 setwd("C:/Getting & Cleaning Data/Week 4/Project")
@@ -8,7 +8,7 @@ setwd("C:/Getting & Cleaning Data/Week 4/Project")
 #read in csv files for X, Y, and Subject Test that were imported into Excel from the provided UCI text files
 #x_test is the raw data results for each subject with 561 variables per row
 #y_test show what activity was being performed during each sample per row
-#subject_test is the subject number in the sample run - 1-30 subjects (people)
+#sub_test is the subject number in the sample run - 1-30 subjects (people)
 X_Test <- data.frame()
 X_Test <- read.csv("./UCI HAR Dataset/test/x_test.csv")
 Y_Test <- data.frame()
@@ -19,6 +19,10 @@ Sub_Test <- read.csv("./UCI HAR Dataset/test/subject_test.csv")
 #read in features.csv converted from text to create column names for X Test/Train dataset
 Features <- data.frame()
 Features <- read.csv("./UCI HAR Dataset/features.csv", check.names = FALSE, header = FALSE)
+
+#read in activity labels.csv converted from text to rename for descriptive names later in final dataset
+Act_Names <- data.frame()
+Act_Names <- read.csv("./UCI HAR Dataset/activity_labels.csv", check.names = FALSE, header = FALSE)
 
 #remove numeric characters before the space in each variable name
 Features <- sub(".+? ", "", Features$V1)
@@ -32,15 +36,6 @@ colnames(X_Test) <- c(Features[1, 1:561])
 
 #subset and filter for columns with only mean, Mean, std, or Std in column names
 filtered_Test <- X_Test[,grepl("mean|Mean|std|Std", names(X_Test))]
-
-#replace column data in Y_test for descriptive names using the activity labels text file from UCI dataset
-#where:  1=WALKING, 2=WALKING_UPSTAIRS, 3=WALKING_DOWNSTAIRS, 4=SITTING, 5=STANDING, 6=LAYING
-Y_Test$X5[Y_Test$X5 == 1] <- "WALKING"
-Y_Test$X5[Y_Test$X5 == 2] <- "WALKING_UPSTAIRS"
-Y_Test$X5[Y_Test$X5 == 3] <- "WALKING_DOWNSTAIRS"
-Y_Test$X5[Y_Test$X5 == 4] <- "SITTING"
-Y_Test$X5[Y_Test$X5 == 5] <- "STANDING"
-Y_Test$X5[Y_Test$X5 == 6] <- "LAYING"
 
 #column bind the three files created above to create a new "imported test" file
 imported_Test <- cbind(Y_Test, filtered_Test)
@@ -77,15 +72,6 @@ colnames(X_Train) <- c(Features[1, 1:561])
 #subset and filter for columns with only mean or std in column names
 filtered_Train <- X_Train[,grepl("mean|Mean|std|Std", names(X_Train))]
 
-#rename column data in Y_test for descriptive names using the activity labels text file from UCI dataset
-#where:  1=WALKING, 2=WALKING_UPSTAIRS, 3=WALKING_DOWNSTAIRS, 4=SITTING, 5=STANDING, 6=LAYING
-Y_Train$X5[Y_Train$X5 == 1] <- "WALKING"
-Y_Train$X5[Y_Train$X5 == 2] <- "WALKING_UPSTAIRS"
-Y_Train$X5[Y_Train$X5 == 3] <- "WALKING_DOWNSTAIRS"
-Y_Train$X5[Y_Train$X5 == 4] <- "SITTING"
-Y_Train$X5[Y_Train$X5 == 5] <- "STANDING"
-Y_Train$X5[Y_Train$X5 == 6] <- "LAYING"
-
 #column bind the three files created above to create a new "imported test" file
 imported_Train <- cbind(Y_Train, filtered_Train)
 imported_Train <- cbind(Sub_Train, imported_Train)
@@ -97,15 +83,9 @@ write.csv(imported_Train, file = "./UCI HAR Dataset/train/imported_train.csv")
 names(imported_Train)[1] <- "Subject_Number"
 names(imported_Train)[2] <- "Activity"
 
-#save filtered test data to a csv file to create a small data set prior to merging with train data
-write.csv(imported_Train, file = "imported_train.csv")
-
 #merge data sets but ensure we only use common columns names to avoid issues with rbind
 cols <- intersect(colnames(imported_Test), colnames(imported_Train))
 mergedBodyData <- rbind(imported_Test[,cols], imported_Train[,cols])
-
-#save merged data to a csv file for later inspection
-write.csv(mergedBodyData, file = "merged_bodydata.csv")
 
 #now need to sort merged datasets based on activity and subject number
 #sort data using order function
@@ -123,62 +103,27 @@ for (i in 1:30) {
 
 #filter data for each subject number
 subjectData_i <- data.frame()
+finalSubjectData_i <- data.frame()
 subjectData_i <- ordered_Bodydata[ordered_Bodydata$Subject_Number == i,]
 
-#then split into separate datasets for each activity type
-subjectLaying_i <- subjectData_i[subjectData_i$Activity == 'LAYING',]
-subjectSitting_i <- subjectData_i[subjectData_i$Activity == 'SITTING',]
-subjectStanding_i <- subjectData_i[subjectData_i$Activity == 'STANDING',]
-subjectWalking_i <- subjectData_i[subjectData_i$Activity == 'WALKING',]
-subjectWalkingDown_i <- subjectData_i[subjectData_i$Activity == 'WALKING_DOWNSTAIRS',]
-subjectWalkingUp_i <- subjectData_i[subjectData_i$Activity == 'WALKING_UPSTAIRS',]
+  #add second for loop for processing activity numbers (1-6) in each subject number run
+  for (isub in 1:6) {
+    
+    #read in each activity number per subject, calcuate mean, and transpose data back in row format
+    activityData_isub <- subjectData_i[subjectData_i$Activity == isub,] 
+    meansubject_isub <- colMeans(activityData_isub)
+    transposeSubject_isub <- t(meansubject_isub)
+  
+    #recursively rbind each mean tidy data set for each subject number
+    finalSubjectData_i <- rbind(finalSubjectData_i, transposeSubject_isub)
+    
+  }
 
-#remove first two columns of integer data that are not required for mean calculation
-#these were added columns to give context to the subject number and activity type
-subjectLaying_i = subjectLaying_i[, -1]
-subjectLaying_i = subjectLaying_i[, -1]
-subjectSitting_i = subjectSitting_i[, -1]
-subjectSitting_i = subjectSitting_i[, -1]
-subjectStanding_i = subjectStanding_i[, -1]
-subjectStanding_i = subjectStanding_i[, -1]
-subjectWalking_i = subjectWalking_i[, -1]
-subjectWalking_i = subjectWalking_i[, -1]
-subjectWalkingDown_i = subjectWalkingDown_i[, -1]
-subjectWalkingDown_i = subjectWalkingDown_i[, -1]
-subjectWalkingUp_i = subjectWalkingUp_i[, -1]
-subjectWalkingUp_i = subjectWalkingUp_i[, -1]
-
-#calculate the means for each column for the dataset broken down by activity type
-#perform required calculations on columns - run colMeans
-meansubjectLaying_i <- colMeans(subjectLaying_i)
-meansubjectSitting_i <- colMeans(subjectSitting_i)
-meansubjectStanding_i <- colMeans(subjectStanding_i)
-meansubjectwalking_i <- colMeans(subjectWalking_i)
-meansubjectWalkingDown_i <- colMeans(subjectWalkingDown_i)
-meansubjectwalkingUp_i <- colMeans(subjectWalkingUp_i)
-
-#transpost data to put back into column orientation format after colMeans converted to single column
-transpose_subjectLaying_i <- t(meansubjectLaying_i)
-transpose_subjectSitting_i <- t(meansubjectSitting_i)
-transpose_subjectStanding_i <- t(meansubjectStanding_i)
-transpose_subjectWalking_i <- t(meansubjectwalking_i)
-transpose_subjectWalkingDown_i <- t(meansubjectWalkingDown_i)
-transpose_subjectWalkingUp_i <- t(meansubjectwalkingUp_i)
-
-#recombine into a single dataset for each subject number
-recombine_subjectData_i <- rbind(transpose_subjectLaying_i, transpose_subjectSitting_i,
-                                transpose_subjectStanding_i,transpose_subjectWalking_i,
-                                transpose_subjectWalkingDown_i, transpose_subjectWalkingUp_i)
-
-#create data frame for mean data for each activity type
-finalTidyMeanData_i <- data.frame()
-
-#add columns for Subject Number and Activity
-finalTidyMeanData_i <- cbind(Subject_Number = i, Activity = c('Laying', 'Sitting', 'Standing', 
-                        'Walking', 'Walking_Downstairs', 'Walking_Upstairs'), recombine_subjectData_i)
+#rename column data in finalSubjectData_i for descriptive names using the activity labels file from UCI dataset
+finalSubjectData_i$Activity <- Act_Names$V2
 
 #recursively rbind each mean tidy data set for each subject number
-finalTidyDataSet <- rbind(finalTidyDataSet, finalTidyMeanData_i)
+finalTidyDataSet <- rbind(finalTidyDataSet, finalSubjectData_i)
 
 #recursively update the final dataset file for each subject number and save file for inspection
 #file should contain six activity rows for each of the 30 subject numbers (people) (180 total data rows)
